5038dc123201f585e77264287ccbc02662b1dbbf
Rmartinez75 <rmartinez75@gmail.com>
2023-08-18 11:43:33 -0400

    Still messing with positions

--- components/PantsComp.jsx
+++ components/PantsComp.jsx
@@ -15,7 +15,7 @@ function PantsComp() {
         <div className='p-0.5 rounded-xl'>
             <Image src='/assets/images/DSC_0110.jpeg' alt='VEST-3' width={400} height={250} />
         </div> 
-        <div className='box-content h-auto w-1/4 ml-auto mr-5' style={{fontSize: '12px'}}>            
+        <div className='box-content h-auto w-1/4 ml-[5%] mr-5' style={{fontSize: '12px'}}>            
             The Virtual Reflection Air Vest is a wearable technology device designed by Metaphysical Abstract Design. This device was originally designed to house VR technologies. It can easily accommodate other wearable technologies with its access pockets and wire conduits, allowing users to use this vest as a prototype for their own ideas. The VR Air Vest comes equipt with an environmental carbon sensor enabling users to access both indoor and outdoor carbon levels. When you put on a VR Air Vest your data counts. You gain access to a community of data by users just like you around the world.
           <br />
           <br />
--- components/VestComp.jsx
+++ components/VestComp.jsx
@@ -16,7 +16,7 @@ function VestComp() {
         <div className='p-0.5 rounded-xl'>
             <Image src='/assets/images/DSC_0071.jpeg' alt='VEST-3' width={400} height={250} />
         </div>
-        <div className='box-content h-auto w-1/4 ml-auto mr-5'>
+        <div className='box-content h-auto w-1/4 ml-[5%] mr-5'>
           <div style={{fontSize: '12px'}}>
           The Virtual Reflection Air Vest is a wearable technology device designed by Metaphysical Abstract Design. This device was originally designed to house VR technologies. It can easily accommodate other wearable technologies with its access pockets and wire conduits, allowing users to use this vest as a prototype for their own ideas. The VR Air Vest comes equipt with an environmental carbon sensor enabling users to access both indoor and outdoor carbon levels. When you put on a VR Air Vest your data counts. You gain access to a community of data by users just like you around the world.
           <br />
