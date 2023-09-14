be2e313cd7e0e6f589d5cfb274c8839185f85f64
Rmartinez75 <rmartinez75@gmail.com>
2023-09-14 10:34:50 -0400

    Test 6 carousel size

--- components/vest/VestCarousel.jsx
+++ components/vest/VestCarousel.jsx
@@ -13,7 +13,7 @@ const slides = [
 function VestCarousel() {
   
   return (
-    <div className='box-content h-auto w-[335px]'>
+    <div className='box-content h-auto w-[330px]'>
       <Carousel autoSlide={true}>
         {slides.map((s, i) => (
           <img src={s} key={i}/>
