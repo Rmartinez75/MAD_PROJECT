c52213b0dcbcb60781b07d300e94e2dbfaff9ba2
Rmartinez75 <rmartinez75@gmail.com>
2024-01-05 12:56:51 -0500

    About page updated

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -3,15 +3,20 @@ import Image from "next/image"
 
 const page = () => {
   return (
-    <div style={{background: '#FFF3F3'}}>
-      
-      <div className="flex justify-center pt-40 pb-[164px] ">
-        
-          <div style={{fontSize: 15, width: '23rem', textAlign: 'center'}} >
-            <div className="flex justify-center"><Image src='/assets/images/g42.png' alt="LOGO" width={90} height={66} />
-            </div>Virtual Reflection (VR) originated as an idea of developing a coherent tool that would bridge the gap between our physical and virtual realities. This tool progressively became a soft and flexible technology through integrating techniques using textiles as its bases. The finish products are a unique and balanced blend of intricate sewing and methodic technology placements, transparency being the key.</div>
+    <div className="flex justify-end" style={{background: '#FFF3F3'}}>      
+      <div className="flex justify-center pr-60 pt-72 pb-[250px] ">        
+          <div style={{fontSize: 15, width: '39rem', textAlign: 'left'}} >
+            <div className="flex justify-center"><Image src='/assets/images/about-image.png' alt="LOGO" width={200} height={66} />
+            <div className="ml-10 mt-3">
+                VR: Virtual Reflection, a brand under
+                Metaphysical Abstract Design LLC,
+                representing the people and their
+                virtual identity.
+            </div>
+            </div>
+            
+          </div>
       </div>
-
     </div>
   )
 }
