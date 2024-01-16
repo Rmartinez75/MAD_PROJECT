6618b1ef4bb8f47dc79ab15623bb11c02fd551c2
Rmartinez75 <rmartinez75@gmail.com>
2024-01-15 21:08:03 -0500

    Deleted unused images and added links on images

--- components/vest/bubble-black/BubbleBlackComp.jsx
+++ components/vest/bubble-black/BubbleBlackComp.jsx
@@ -12,33 +12,35 @@ function BubbleBlackComp() {
     <div id='vest-bubbleblack-section'>     
       <div className='hidden lg:flex ml-3'> 
         <div className='p-0.5'>
+          <Link href={'/bubbleBlack'}>
             <img src='/assets/images/VR_AIR_BUBBLE_BLACK/square1.png' alt='VEST-1' className='w-[400px]' />
-          </div>       
-          <div className='p-0.5'>
+          </Link>
+        </div>       
+        <div className='p-0.5'>
+          <Link href={'/bubbleBlack'}>
             <img src='/assets/images/VR_AIR_BUBBLE_BLACK/square2.png' alt='VEST-2' className='w-[400px]' />
-          </div>
-          <div className='p-0.5 w-[404px]'>            
-            <BubbleBlackCarousel />                            
-          </div>
-          <div className='box-content h-auto w-1/4 ml-[5%]'>
-            <div style={{fontSize: '12px'}}>
-              <Link href={'/bubbleBlack'} className='font-bold'>VR AIR: BUBBLE BLACK</Link> 
-              <br />
-              <br />
-              This device was original designed to house VR technologies and can accommodate other wearable technologies, with its access pockets and wire conduits, allowing users with the possibility of testing out their own ideas. The VR Air Vest comes equipped with an environmental carbon sensor allowing users to get accurate indoor and outdoor readings.
+          </Link>
+        </div>
+        <div className='p-0.5 w-[404px]'>            
+          <BubbleBlackCarousel />                            
+        </div>
+        <div className='box-content h-auto w-1/4 ml-[5%]'>
+          <div style={{fontSize: '12px'}}>
+            <Link href={'/bubbleBlack'} className='font-bold'>VR AIR: BUBBLE BLACK</Link> 
             <br />
             <br />
-            <Counter />
-            </div>          
+            This device was original designed to house VR technologies and can accommodate other wearable technologies, with its access pockets and wire conduits, allowing users with the possibility of testing out their own ideas. The VR Air Vest comes equipped with an environmental carbon sensor allowing users to get accurate indoor and outdoor readings.
+          <br />
+          <br />
+          <Counter />
+          </div>          
         </div>
       </div>
       <br />
       <div className='hidden lg:flex justify-between'>
         <VestSchematics />
       </div>
-
-      <BubbleBlackMobileCarousel />
-      
+      <BubbleBlackMobileCarousel />      
     </div>
   )
 }
