f88f94b29a677a0a76cc2018072605495db551ea
Rmartinez75 <rmartinez75@gmail.com>
2024-01-18 23:01:06 -0500

    Continuing size adjustments

--- app/bubbleBlack/page.jsx
+++ app/bubbleBlack/page.jsx
@@ -162,8 +162,8 @@ function page() {
   return (
     <div className="pages" style={{width: '100%'}}>
       <div className="flex justify-center">
-        <div className="block w-auto ml-10">
-          <div className="flex w-auto">
+        <div className="block w-[500px] lg:w-auto ml-10">
+          <div className="flex max-w-screen-2xl">
             <div className="w-[900px] mt-8 mb-2">
               <LargeImageComp />          
             </div>
@@ -181,7 +181,7 @@ function page() {
           </div>
         </div>
         <div className='w-auto ml-5 mr-10'>
-          <div className="mt-8 " style={{ height: '1050px', width:'100%', overflow: 'scroll'}}>
+          <div className="mt-8" style={{ height: '1050px', overflow: 'scroll'}}>
             <p className="font-bold" style={{fontSize: '14px'}}>
               VR: AIRVEST
             </p>
--- tailwind.config.js
+++ tailwind.config.js
@@ -16,10 +16,10 @@ module.exports = {
         'sm': '468px',
         // => @media (min-width: 640px) { ... }
   
-        'md': '960px',
+        'md': '768px',
         // => @media (min-width: 768px) { ... }
   
-        'lg': '1080px',
+        'lg': '1024px',
         // => @media (min-width: 1024px) { ... }
   
         'xl': '1280px',
