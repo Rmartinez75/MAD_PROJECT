3f5fb5f9f30bb022dfd0dceaaf6fa29e4eed8bfe
Rmartinez75 <rmartinez75@gmail.com>
2024-03-25 16:03:14 -0400

    Google Analytics script added

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,6 @@
 
 import Head from 'next/head'
-// import ScrollToTopButton from '@components/ScrollToTopButton'
+import Script from 'next/script'
 
 
 import '@styles/globals.css'
@@ -15,6 +15,18 @@ const RootLayout = ({ children }) => {
     <html lang="en">
       <Head>
         <link rel="icon" href='/favicon.ico' />
+
+        <Script strategy='lazyOnload' src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS}`}></Script> 
+
+        <Script id='ga-script' strategy='lazyOnload'> 
+          {`
+            window.dataLayer = window.dataLayer || []; 
+            function gtag(){dataLayer.push(arguments);} 
+            gtag('js', new Date()); 
+            gtag('config', '${process.env.GOOGLE_ANALYTICS}'); 
+          `}
+        </Script>
+
       </Head>
       <body>
         <main>
