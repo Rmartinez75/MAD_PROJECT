20437530421297a9dc219b5de2f9694a0e720d76
rmartinez75 <rmartinez75@gmail.com>
2024-05-15 21:17:14 -0400

    Coming soon page fix white space 4

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -9,7 +9,7 @@ const DashboardPage = async () => {
     const user = await getUser();
   return (
     <div style={{background: '#FFF3F3'}}>
-      <div className='flex text-center justify-center text-[11px]' >
+      <div className='flex h-screen text-center justify-center items-center text-[11px]' >
         <div className='flex mt-[15%] mb-[15%]'>
           <span className='mt-[35px]'>IN PROGRESS</span>
           <Image src={'/assets/images/about-image.png'} width={125} height={75} alt='Image' className='ml-8 mr-8'/>
