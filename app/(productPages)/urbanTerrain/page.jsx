cf58f585536486ceb24083747be797b08eb09a00
rmartinez75 <rmartinez75@gmail.com>
2024-06-23 13:40:21 -0400

    Description troubleshooting

--- app/(productPages)/urbanTerrain/page.jsx
+++ app/(productPages)/urbanTerrain/page.jsx
@@ -124,7 +124,7 @@ function page() {
             <br />
             <br />
             <br />
-            <p style={{fontSize: '12px'}}>SIZE:&nbsp; &#160;&nbsp; &#160;&nbsp; &#160; TYPE:&nbsp; &#160;&nbsp; &#160;&nbsp; &#160; <RegisterLink postLoginRedirectURL='/dashboard' className='flex'>GET NOTIFIED<FaRegRegistered className='ml-3 mt-1' /></RegisterLink>:&#160;&nbsp; EMAIL HERE</p>
+            {/* <p style={{fontSize: '12px'}}>SIZE:&nbsp; &#160;&nbsp; &#160;&nbsp; &#160; TYPE:&nbsp; &#160;&nbsp; &#160;&nbsp; &#160; <RegisterLink postLoginRedirectURL='/dashboard' className='flex'>GET NOTIFIED<FaRegRegistered className='ml-3 mt-1' /></RegisterLink>:&#160;&nbsp; EMAIL HERE</p> */}
             
             <br />
             <br />
